f5233a931cb81d8ac3f12c995a5ffa7e90566e16
Tiago Santos <tiago@convertodigital.com>
2020-02-17 14:30:40 +0000

    Stupid Coaching

--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,4 @@ Rails.application.routes.draw do
   get 'answer', to: 'questions#answer', as: :answer
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
